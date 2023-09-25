ecd89b691d25c0294df6b1c4c8367258b11329bc
sdhairya <sdhairya412@gmail.com>
2023-09-25 21:42:46 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
         primarySwatch: Colors.blue,
 
